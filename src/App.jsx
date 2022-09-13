7ff7375a268a2dbbc27c6722c99fee320d8b9bba
Braian Gomez <braiandenisgomez@gmail.com>
2022-09-12 23:19:47 -0300

    Created characterList component with fetch  useeffect, characters state,  and  created character component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import CharacterList from "./components/CharacterList";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Rick and morty</h1>
+      <CharacterList />
+    </div>
+  );
+};
+
+export default App;
+
